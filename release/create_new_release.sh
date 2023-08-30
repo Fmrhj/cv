9e0917678d96d6e796b812db2d423a52cf86a310
Fernando Zepeda <ferna.zepeda@gmail.com>
2023-08-30 20:35:24 +0200

    Feature/automate releases (#5)
    - automate release process

---
+++ release/create_new_release.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -e
+
+bump_type=$1
+
+git fetch --tags origin
+curr_tag="`git describe --abbrev=0 --tags 2>/dev/null`"
+
+if [ "$bump_type" == "chore" ]; then 
+    echo $curr_tag 
+    exit 0
+fi
+
+if [[ $curr_tag == '' ]]; then
+    curr_tag='v0.1.0'
+fi
+
+echo "Current tag:"
+echo $curr_tag
+
+curr_version=${curr_tag/v/}
+
+curr_version_bits=(${curr_version//./ })
+
+curr_major=$((${curr_version_bits[0]}))
+curr_minor=$((${curr_version_bits[1]}))
+curr_patch=$((${curr_version_bits[2]}))
+
+elif [ "$bump_type" == "major" ] || ([ "$curr_minor" == 999 ] && [ "$curr_patch" == 999 ]); then
+    ((curr_major++))
+    curr_minor=0
+    curr_patch=0
+elif [ "$bump_type" == "minor" ] || [ "$curr_patch" == 999 ]; then
+    ((curr_minor++))
+    curr_patch=0
+else
+    ((curr_patch++))
+fi
+
+new_tag="v${curr_major}.${curr_minor}.${curr_patch}"
+
+GIT_COMMIT=`git rev-parse HEAD`
+NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
+
+echo "Tagged with:"
+echo $new_tag
